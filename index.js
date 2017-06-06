69766009f38a8ca382e27b1a48632442f3e47c03
Alec Montalbano <alec.montalbano@gmail.com>
2017-06-06 18:21:37 -0400

    implemented server functionality utilizing express and tested using html and css

---
+++ index.js
@@ -0,0 +1,16 @@
+/* jshint esversion:6 */
+
+const express = require('express');
+const server = express();
+
+const port = 8080;
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/rerecounter', (request, response) => {
+  response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.listen(port, () => {
+  console.log('Listening on ', port);
+});
